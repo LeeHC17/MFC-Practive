7d66153e8c6998a5c6adb35a2519012ea4c52570
LeeHC17 <lhchang9@naver.com>
2021-07-02 17:22:15 +0900

    update 7/2

---
+++ ModalDialog/resource.h
@@ -0,0 +1,27 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// ModalDialog.rc에서 사용되고 있습니다.
+//
+#define IDOK2                           3
+#define IDCANCEL2                       4
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_ModalDialogTYPE             130
+#define IDD_DIALOG1                     310
+#define IDC_STR                         1000
+#define IDC_FONT                        1001
+#define IDC_BUTTON1                     1002
+#define IDC_CLEAR                       1002
+#define IDC_FONT2                       1003
+#define IDC_BUTTON2                     1004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        313
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
---
+++ ModelessDialog/CMyDialog.cpp
@@ -0,0 +1,69 @@
+﻿// CMyDialog.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "ModelessDialog.h"
+#include "CMyDialog.h"
+#include "afxdialogex.h"
+#include "ModelessDialogDoc.h"
+#include "ModelessDialogView.h"
+
+
+// CMyDialog 대화 상자
+
+IMPLEMENT_DYNAMIC(CMyDialog, CDialog)
+
+CMyDialog::CMyDialog(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_DIALOG1, pParent)
+	, m_str(_T(""))
+	, m_font(0)
+{
+
+}
+
+CMyDialog::~CMyDialog()
+{
+}
+
+void CMyDialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_STR, m_str);
+	DDV_MaxChars(pDX, m_str, 10);
+	DDX_CBIndex(pDX, IDC_FONT, m_font);
+}
+
+
+BEGIN_MESSAGE_MAP(CMyDialog, CDialog)
+	ON_BN_CLICKED(IDAPPLY, &CMyDialog::OnBnClickedApply)
+END_MESSAGE_MAP()
+
+
+// CMyDialog 메시지 처리기
+
+
+void CMyDialog::OnBnClickedApply()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData(TRUE);
+	m_pView->m_str = m_str;
+	m_pView->m_font = m_font;
+	m_pView->Invalidate();
+}
+
+
+void CMyDialog::OnCancel()
+{
+	// TODO: 여기에 특수화된 코드를 추가 및/또는 기본 클래스를 호출합니다.
+	DestroyWindow();
+	//CDialog::OnCancel();
+}
+
+
+void CMyDialog::PostNcDestroy()
+{
+	// TODO: 여기에 특수화된 코드를 추가 및/또는 기본 클래스를 호출합니다.
+	m_pView->m_pDlg = NULL;
+	delete this;
+	//CDialog::PostNcDestroy();
+}
