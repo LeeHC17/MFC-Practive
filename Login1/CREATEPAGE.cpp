786285048f95fe7e49abbb6460313653b04162ad
LeeHC17 <lhchang9@naver.com>
2021-07-06 18:38:59 +0900

    update 7/6

---
+++ Login1/CREATEPAGE.cpp
@@ -0,0 +1,62 @@
+﻿// CREATEPAGE.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "Login1.h"
+#include "CREATEPAGE.h"
+#include "afxdialogex.h"
+
+
+// CREATEPAGE 대화 상자
+
+IMPLEMENT_DYNAMIC(CREATEPAGE, CDialogEx)
+
+CREATEPAGE::CREATEPAGE(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+	
+}
+
+CREATEPAGE::~CREATEPAGE()
+{
+}
+
+void CREATEPAGE::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_EDIT1, m_Edit_ID);
+	DDX_Control(pDX, IDC_EDIT2, m_Edit_PASS);
+}
+
+
+BEGIN_MESSAGE_MAP(CREATEPAGE, CDialogEx)
+	ON_BN_CLICKED(IDC_CREATE, &CREATEPAGE::OnBnClickedCreate)
+END_MESSAGE_MAP()
+
+
+// CREATEPAGE 메시지 처리기
+
+
+void CREATEPAGE::OnBnClickedCreate()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	CString account, str_id, str_pass;
+
+	m_Edit_ID.GetWindowText(str_id);
+	m_Edit_PASS.GetWindowText(str_pass);
+
+	account = str_id + " " + str_pass;
+
+	CFile file;
+	file.Open(_T("account.txt"), CFile::modeCreate | CFile::modeNoTruncate | CFile::modeWrite);
+
+	file.Write(account, account.GetLength() * sizeof(char) * 2);
+	MessageBox(_T("Create account success!!"), _T("Success"), NULL);
+	file.Close();
+
+
+	/*CArchive Account(&file, CArchive::store);
+	Account << account;
+	MessageBox(_T("Create account success!!"), _T("Success"), NULL);
+	*/
+}
---
+++ Login1/Login1Dlg.h
@@ -0,0 +1,40 @@
+﻿
+// Login1Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CLogin1Dlg 대화 상자
+class CLogin1Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CLogin1Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_LOGIN1_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+	CEdit m_EDIT_ID;
+	CEdit m_EDIT_PASS;
+public:
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnBnClickedCreatbtn();
+	afx_msg void OnBnClickedLoginbtn();
+};
